6ba14404950cb9bfd9c49d6a76c85c9fcd413cb5
Pathum-SC12402 <fernando12402@usci.ruh.ac.lk>
2025-06-02 00:17:38 +0530

    Added basic configurations using NuGet package manager

--- obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c389b151bec089a4e67ba272c996b95076679631")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
