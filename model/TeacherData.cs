ffb7bc1c9c1a3f0e8651209f5d4284aa7a5e8f47
Pathum-SC12402 <fernando12402@usci.ruh.ac.lk>
2025-06-10 12:24:14 +0530

    teacher data model was created

--- model/TeacherData.cs
+++ model/TeacherData.cs
@@ -7,6 +7,21 @@ namespace backend.model
 {
     public class TeacherData
     {
-        
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Age { get; set; }
+        public required string Gender { get; set; }
+        public required string Address { get; set; }
+        public required string SpecializedArea { get; set; }
+        public required string TeachingSide { get; set; }
+        public string CV { get; set; } = string.Empty;
+        public string ProfilePicture { get; set; } = string.Empty;
+        public string CoverPicture { get; set; } = string.Empty;
+        public required string Description { get; set; }
+        public int TeacherAccountId { get; set; }
+        public required TeacherAccount TeacherAccount { get; set; } = null!;
+        public List<TeacherCertificates> Certificates { get; set; } = new List<TeacherCertificates>();
     }
 }
